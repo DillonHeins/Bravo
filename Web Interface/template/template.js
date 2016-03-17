4bfead687b5245ff2080ef4a94c1fc6a6dd297b7
Diana <diana.kawuma@gmail.com>
2016-03-17 19:39:08 +0200

    updated mock objects to template.js file

--- Web Interface/template/template.js
+++ Web Interface/template/template.js
@@ -18,3 +18,25 @@ function init() {
 	$("#listUsersOutput").html(theTemplate(usersArray)); // add the compiled html code to the body
 }
 
+/*Reporting section*/
+
+var reportArray = { // this is the mock object
+	title: "Select lifecycle states", // this is the title that will be used
+	states: [{ 
+		state: "inProgress"
+	}, { 
+		state: "Submitted"
+	}, { 
+		state: "Accepted"
+	}, { 
+		state: "InRevision"
+	}, {
+		state: "Abandoned"
+	}]
+};
+
+function initReport() {
+	var theTemplateScript = $("#list-states").html();   // get the content of what we want to compile, i.e. the content of the handlebar script secion in index.html
+	var theTemplate = Handlebars.compile(theTemplateScript); // compile the handlebars template
+	$("#listStates").html(theTemplate(reportArray)); // add the compiled html code to the body
+}
