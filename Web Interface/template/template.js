a570f481aca73ceea4f9fd12df251e1b8c01705d
Armand Maree <aamaree@gmail.com>
2016-03-17 16:22:56 +0200

    Added a template for Handlebars

---
+++ Web Interface/template/template.js
@@ -0,0 +1,20 @@
+var usersArray = { // this is the mock object
+	title: "List of collaborators", // this is the title that will be used
+	users: [{ // this is the array that will be looped
+		firstName: "John",
+		lastName: "Doe"
+	}, {
+		firstName: "Sarah",
+		lastName: "Smith"
+	}, {
+		firstName: "Some",
+		lastName: "Person"
+	}]
+};
+
+function init() {
+	var theTemplateScript = $("#list-users").html();   // get the content of what we want to compile, i.e. the content of the handlebar script secion in index.html
+	var theTemplate = Handlebars.compile(theTemplateScript); // compile the handlebars template
+	$("#listUsersOutput").html(theTemplate(usersArray)); // add the compiled html code to the body
+}
+
