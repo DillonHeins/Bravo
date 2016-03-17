1d3ae85be13eb39a833abb41a5bc92f9559c2671
Diana <diana.kawuma@gmail.com>
2016-03-17 23:10:39 +0200

    added mock objects for viewing report section

--- Web Interface/template/template.js
+++ Web Interface/template/template.js
@@ -18,10 +18,10 @@ function init() {
 	$("#listUsersOutput").html(theTemplate(usersArray)); // add the compiled html code to the body
 }
 
-/*Reporting section*/
+/*generating reporting section*/
 
-var reportArray = { // this is the mock object
-	title: "Select lifecycle states", // this is the title that will be used
+var reportArray = { 
+	title: "Select lifecycle states", 
 	states: [{ 
 		state: "inProgress"
 	}, { 
@@ -36,7 +36,40 @@ var reportArray = { // this is the mock object
 };
 
 function initReport() {
-	var theTemplateScript = $("#list-states").html();   // get the content of what we want to compile, i.e. the content of the handlebar script secion in index.html
-	var theTemplate = Handlebars.compile(theTemplateScript); // compile the handlebars template
-	$("#listStates").html(theTemplate(reportArray)); // add the compiled html code to the body
+	var theTemplateScript = $("#list-states").html();   
+	var theTemplate = Handlebars.compile(theTemplateScript); 
+	$("#listStates").html(theTemplate(reportArray));
+}
+
+/* viewing report section*/
+var reportViewArray = { 
+	title: "Results", 
+	report: [{ 
+        number:"1",
+        group: "Alpha",
+		pupType: "Conference paper",
+		ProgStat: "60%",
+        state: "inProgress",
+        dueDate: "24/04/16"
+	}, { 
+        number:"2",
+		group: "Olmega",
+		pupType: "Journal",
+		ProgStat: "30%",
+        state: "inProgress",
+        dueDate: "1/04/16"
+	}, { 
+        number:"3",
+		group: "Bravo",
+		pupType: "Book",
+		ProgStat: "90%",
+        state: "inProgress",
+        dueDate: "31/03/16"
+	}]
+};
+
+function initReportView() {
+	var theTemplateScript = $("#list-report").html();   
+	var theTemplate = Handlebars.compile(theTemplateScript); 
+	$("#listDetails").html(theTemplate(reportViewArray));
 }
