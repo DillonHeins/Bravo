4e21d814ff6d8a97e9ce839b50f89e588b81ab0d
Dillon Heins <dillonheins@gmail.com>
2016-03-18 04:43:38 +0200

    Maven build test

---
+++ Maven/app/Greeter/src/main/java/bravo/Greeter.java
@@ -0,0 +1,16 @@
+package bravo;
+
+/**
+ * Hello world!
+ *
+ */
+public class Greeter 
+{
+	public Greeter() {
+		System.out.println("Creating greeting...");
+	}
+
+    public void display() {
+    	System.out.println("Greetings!");
+    }
+}
---
+++ Maven/app/Main/src/main/java/bravo/App.java
@@ -0,0 +1,13 @@
+package bravo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
