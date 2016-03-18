51d18df3bfe1f0ee43f379e0f5a6aa41992e91b5
Dillon Heins <dillonheins@gmail.com>
2016-03-18 11:13:49 +0200

    Fixed uber jar compilation

---
+++ Maven/app/App/src/main/java/bravo/App.java
@@ -0,0 +1,16 @@
+package bravo;
+import bravo.Greeter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Greeter greeter = new Greeter();
+        greeter.display();
+    }
+}
