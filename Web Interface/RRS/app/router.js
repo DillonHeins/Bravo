468af8a05a054e99085f1b20f608e466f43579e9
Armand Maree <aamaree@gmail.com>
2016-03-24 17:38:24 +0200

    Merge Reporting

--- Web Interface/RRS/app/router.js
+++ Web Interface/RRS/app/router.js
@@ -6,16 +6,17 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  //TODO: Create a route and add sub routes to it. E.g.:
-  // this.route('publication', {path: '/publication'}, function () { // we have publications and as a sub route we have the add and edit pages
-  //   this.route('add');
-  //   this.route('edit');
-  // });
   this.route('home', {path: '/'});
 
-  this.route('reporting', { path: '/' }, function() {
-    this.route('query');
-    this.route('view');
+  //TODO: Create a route and add sub routes to it. E.g.:
+  // this.route('publication', {path: '/publication'}, function () { // we have publications and as a sub route we have the add and edit pages
+  //   this.route('add', { path: '/publication/add'});
+  //   this.route('edit', { path: '/publication/edit'});
+  // });
+
+  this.route('reporting', { path: '/reporting' }, function() {
+    this.route('query', { path: '/reporting/query' });
+    this.route('view', { path: '/reporting/view' });
   });
 });
 
