5211522b08b0e30e61ed8ae3881d5582dfa7b7e7
Armand Maree <aamaree@gmail.com>
2016-03-24 17:04:22 +0200

    Added RRS/app and updated .gitignore

---
+++ Web Interface/RRS/app/router.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+import config from './config/environment';
+
+const Router = Ember.Router.extend({
+  location: config.locationType
+});
+
+Router.map(function() {
+  this.route('home', {path: '/'});
+  //TODO: Create a route and add sub routes to it. E.g.:
+  // this.route('publication', {path: '/publication'}, function () { // we have publications and as a sub route we have the add and edit pages
+  //   this.route('add');
+  //   this.route('edit');
+  // });
+});
+
+export default Router;
---
+++ Web Interface/RRS/app/routes/application.js
@@ -0,0 +1,24 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+    model() {
+        let todos = [
+            {
+                name: "Publications",
+                url: "#",
+								sub: [{
+									name: "Add",
+	                url: "/add",
+								}, {
+									name: "Edit",
+	                url: "/edit",
+								}],
+            },
+            {
+								name: "Persons",
+								url: "#",
+            }
+        ];
+        return todos;
+    }
+});
