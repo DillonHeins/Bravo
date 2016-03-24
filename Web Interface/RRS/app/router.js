d32189fd76bb10e79d74178da3aece0a7f4daee9
Diana <diana.kawuma@gmail.com>
2016-03-24 13:27:09 +0200

    added route Reporting and sub-routes
    also added content to the query html page

--- Web Interface/RRS/app/router.js
+++ Web Interface/RRS/app/router.js
@@ -6,13 +6,17 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('home', {path: '/'});
   //TODO: Create a route and add sub routes to it. E.g.:
   // this.route('publication', {path: '/publication'}, function () { // we have publications and as a sub route we have the add and edit pages
   //   this.route('add');
   //   this.route('edit');
   // });
+  this.route('home', {path: '/'});
 
+  this.route('reporting', { path: '/' }, function() {
+    this.route('query');
+    this.route('view');
+  });
 });
 
 export default Router;
---
+++ Web Interface/RRS/tests/unit/routes/reporting/view-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:reporting/view', 'Unit | Route | reporting/view', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
