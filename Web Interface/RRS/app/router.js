711d483859eec081d36a3663866bead849e3e2f9
Jason Gordon <u14405025@tuks.co.za>
2016-03-24 17:38:16 +0200

    Added a file for importing csv

--- Web Interface/RRS/app/router.js
+++ Web Interface/RRS/app/router.js
@@ -6,19 +6,20 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('home', {path: '/'});
   //TODO: Create a route and add sub routes to it. E.g.:
   // this.route('publication', {path: '/publication'}, function () { // we have publications and as a sub route we have the add and edit pages
   //   this.route('add');
   //   this.route('edit');
   // });
-    
-    /* reporting section
-    this.route('reporting', { path: '/' }, function() {
-    this.route('query');
-    this.route('view');
+  this.route('home', {path: '/'});
+
+  /* reporting section
+  this.route('reporting', { path: '/' }, function() {
+  this.route('query');
+  this.route('view');
   });
-    */
+  */;
+  this.route('import');
 });
 
 export default Router;
