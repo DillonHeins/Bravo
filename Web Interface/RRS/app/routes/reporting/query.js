bafc7064138382866b268a5da55c58a3bed883d1
Diana <diana.kawuma@gmail.com>
2016-03-24 13:47:27 +0200

    added handlerbar content to hmtl page

--- Web Interface/RRS/app/routes/reporting/query.js
+++ Web Interface/RRS/app/routes/reporting/query.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    model() {
+        let reportQArray = [
+                { 
+                    state: "inProgress",
+                }, { 
+                    state: "Submitted",
+                }, { 
+                    state: "Accepted",
+                }, { 
+                     state: "InRevision",
+                }, {
+                    state: "Abandoned",
+                }];
+           return reportQArray;
+  } 
 });
