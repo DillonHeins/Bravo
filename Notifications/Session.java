ff0bd1e14a33701892c4e9090029b659a7a0bca9
Azhar Mohungoo <azhar.m.ish@gmail.com>
2016-04-05 23:11:27 +0200

    Session.java helper, and other, functions

--- Notifications/Session.java
+++ Notifications/Session.java
@@ -36,7 +36,7 @@ public class Session
     {
         this(host, 25, recipient, sender, subject, message);
     }
-    
+
     public void close()
     {
         try
@@ -57,6 +57,79 @@ public class Session
 
         in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         out = new OutputStreamWriter(socket.getOutputStream());
+    }
+
+    protected String sendCommand(String commandString) throws IOException
+    {
+        out.write(commandString + "\n");
+        out.flush();
+
+        String response = getResponse();
+
+        return response;
+    }
+
+    protected void doCommand(String commandString, char expectedResponseStart) throws IOException
+    {
+        String response = sendCommand(commandString);
+
+        checkServerResponse(response, expectedResponseStart);
+    }
+
+    protected void checkServerResponse(String response, char expectedResponseStart) throws IOException
+    {
+        if (response.charAt(0) != expectedResponseStart)
+        {
+            throw new IOException(response);
+        }
+    }
+
+    protected String getResponse() throws IOException
+    {
+        String response = "";
+        String line;
+
+        do
+        {
+            line = in.readLine();
+
+            if ((line == null) || (line.length() < 3))
+            {
+                throw new IOException("Bad response from server.");
+            }
+
+            response += line + "\n";
+
+        } while ((line.length() > 3) && (line.charAt(3) == '-'));
+
+        return response;
+    }
+
+    protected String getMessageHeaders()
+    {
+        String headers = "";
+
+        headers += "Date: " + new Date().toString() + "\n";
+        headers += "Sender: " + sender + "\n";
+        headers += "From: " + sender + "\n";
+
+        if (recipient.length == 1)
+        {
+            headers += "To: " + recipient[0] + "\n";
+        }
+        else if (recipient.length > 1)
+        {
+            headers += "To: " + recipient[0] + "\n";
+
+            for (int k = 1; k < recipient.length(); k++)
+            {
+                headers += "Cc: " + recipient[k] + "\n";
+            }
+        }
+
+        headers += "Subject: " + subject + "\n";
+
+        return headers + "\n\n";
     }
 
     public void sendMessage() throws IOException
@@ -102,5 +175,4 @@ public class Session
 
         close();
     }
-
 }
