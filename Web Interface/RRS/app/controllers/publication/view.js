7cb5d030c448ded110535a5885a0db4414dea19b
Armand Maree <aamaree@gmail.com>
2016-04-13 23:11:33 +0200

    - Corrected the export to bib link name generation. - Fixed some spacing issues (CSS)

--- Web Interface/RRS/app/controllers/publication/view.js
+++ Web Interface/RRS/app/controllers/publication/view.js
@@ -1,21 +1,19 @@
 import Ember from 'ember';
 
-$(".exportTo").ready(function()
+$("#pubContainer").ready(function()
 {
 	// alert("hello");
 	var i, t, bibButton;
 	//for ALL the publications viewed on the page, create a possibility to export to CSV. It will be generated when the page has finished rendering
 	bibButton = document.createElement("a");
-	bibButton.setAttribute("class", "btn btn-danger");
-	bibButton.setAttribute("id", "bibtex"); 
-	bibButton.setAttribute("type", "submit"); 
-	var pubname = "{{reportObj.title}}" + ".bib";
+	bibButton.setAttribute("class", "btn btn-danger bibtex");
+	bibButton.setAttribute("type", "submit");
+	var pubname = $(this).find(".pubname").data("name") + ".bib";
 	bibButton.setAttribute("href", pubname);
 	bibButton.setAttributeNode(document.createAttribute("download"));
 	t = document.createTextNode("Export to Bibtex");
 	bibButton.appendChild(t);
 	$(bibButton).appendTo($(".progress-bar[aria-valuenow='100%']").parent().parent());
-	
 });
 
 
@@ -29,5 +27,5 @@ export default Ember.Controller.extend(
 	// 		alert("YAYYAYAYAYAY!!!");
 
 	// 	});
-	// }	
+	// }
 });
