bcf2590baa4e4b3dc4b1afb6a86cf67733a5a5a9
Armand Maree <aamaree@gmail.com>
2016-04-14 13:52:51 +0200

    - Added dynamic redndering of the Export to Bib button

--- Web Interface/RRS/app/controllers/publications/view.js
+++ Web Interface/RRS/app/controllers/publications/view.js
@@ -1,31 +1,6 @@
 import Ember from 'ember';
 
-$("#pubContainer").ready(function()
-{
-	// alert("hello");
-	var i, t, bibButton;
-	//for ALL the publications viewed on the page, create a possibility to export to CSV. It will be generated when the page has finished rendering
-	bibButton = document.createElement("a");
-	bibButton.setAttribute("class", "btn btn-danger bibtex");
-	bibButton.setAttribute("type", "submit");
-	var pubname = $(this).find(".pubname").data("name") + ".bib";
-	bibButton.setAttribute("href", pubname);
-	bibButton.setAttributeNode(document.createAttribute("download"));
-	t = document.createTextNode("Export to Bibtex");
-	bibButton.appendChild(t);
-	$(bibButton).appendTo($(".progress-bar[aria-valuenow='100%']").parent().parent());
-});
-
-
-
 export default Ember.Controller.extend(
 {
-	// didInsertElement: function()
-	// {
-	// 	Ember.run.schedule("afterRender", this, function()
-	// 	{
-	// 		alert("YAYYAYAYAYAY!!!");
 
-	// 	});
-	// }
 });
