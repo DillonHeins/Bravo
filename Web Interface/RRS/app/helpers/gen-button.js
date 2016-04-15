bc953bc50b91e661b897a30a9d2e1f1189187feb
Armand Maree <aamaree@gmail.com>
2016-04-15 07:21:28 +0200

    Accomodate the changes made by recent import merge

--- Web Interface/RRS/app/helpers/gen-button.js
+++ Web Interface/RRS/app/helpers/gen-button.js
@@ -1,4 +1,4 @@
-export function genButton(params) 
+export function genButton(params)
 {
   if(params[0] == params[1])
     return new Ember.Handlebars.SafeString('<a class="btn btn-danger" href="' + params[2] + '.bib" download>Export to Bibtex</a>');
