3d19d74113d77005f9457623bee3477f779d7c7d
Daniel King <nalgaroth@hotmail.com>
2016-04-15 12:55:55 +0200

    Addition of unit tests for sendNotification

--- Notifications/notifications_ejb/src/test/java/bravo/notifications/ejb/NotificationsBeanTest.java
+++ Notifications/notifications_ejb/src/test/java/bravo/notifications/ejb/NotificationsBeanTest.java
@@ -11,120 +11,214 @@ import static org.mockito.Mockito.*;
 import org.mockito.Spy;
 import static org.mockito.Matchers.*;
 
-
 @RunWith(MockitoJUnitRunner.class)
-public class NotificationsBeanTest
-{
+public class NotificationsBeanTest {
+
     @Spy
     private NotificationsBean notificationsBean = new NotificationsBean();
-    
+
     @Test
-    public void testSendReminderBase() throws Exception                                                                                                                     // start of SendReminderTest
+    public void testSendReminderBase() throws Exception // start of SendReminderTest
     {
         // Setup
         doReturn("Test email message").when(notificationsBean).buildMessage(anyString(), anyString());
         doReturn(true).when(notificationsBean).submitTextMail(anyString(), anyString(), anyString());
-        
+
         String[] emailList = new String[1];
         emailList[0] = "u10297902@tuks.co.za";
-        
+
         // Run Test
         boolean result = notificationsBean.sendReminder(emailList);
-        
+
         // Verify Test
         assertTrue(result);
         verify(notificationsBean).buildMessage("u10297902@tuks.co.za", "Reminder");
         verify(notificationsBean).submitTextMail("u10297902@tuks.co.za", "2 Day Reminder", "Test email message");
     }
-    
+
     @Test
-    public void testSendReminderOneEmailFail() throws Exception
-    {
+    public void testSendReminderOneEmailFail() throws Exception {
         // Setup
         doReturn("Test email message").when(notificationsBean).buildMessage(anyString(), anyString());
         doReturn(false).when(notificationsBean).submitTextMail(anyString(), anyString(), anyString());
-        
+
         String[] emailList = new String[2];
         emailList[0] = "u10297902@tuks.co.za";
-        
+
         // Run Test
         boolean result = notificationsBean.sendReminder(emailList);
-        
+
         // Verify Test
         assertFalse(result);
         verify(notificationsBean).buildMessage("u10297902@tuks.co.za", "Reminder");
         verify(notificationsBean).submitTextMail("u10297902@tuks.co.za", "2 Day Reminder", "Test email message");
     }
-    
+
     @Test
-    public void testSendReminderEmptyArray() throws Exception
-    {
+    public void testSendReminderEmptyArray() throws Exception {
         // Setup
         String[] emailList = new String[0];
-        
-         // Run Test
+
+        // Run Test
         boolean result = notificationsBean.sendReminder(emailList);
-        
+
         // Verify Test
         assertFalse(result);
         verify(notificationsBean, never()).buildMessage(anyString(), anyString());
         verify(notificationsBean, never()).submitTextMail(anyString(), anyString(), anyString());
     }
-    
+
     @Test
-    public void testSendReminderNullArray() throws Exception
-    {
-         // Setup
+    public void testSendReminderNullArray() throws Exception {
+        // Setup
         String[] emailList = null;
-        
-         // Run Test
+
+        // Run Test
         boolean result = notificationsBean.sendReminder(emailList);
-        
+
         // Verify Test
         assertFalse(result);
         verify(notificationsBean, never()).buildMessage(anyString(), anyString());
         verify(notificationsBean, never()).submitTextMail(anyString(), anyString(), anyString());
     }
-    
-    @Test(expected=Exception.class)
-    public void testSendReminderExceptionOnBuildMessage() throws Exception
-    {
+
+    @Test(expected = Exception.class)
+    public void testSendReminderExceptionOnBuildMessage() throws Exception {
         // Setup
         when(notificationsBean.buildMessage(anyString(), anyString())).thenThrow(new Exception());
         doReturn(true).when(notificationsBean).submitTextMail(anyString(), anyString(), anyString());
-        
+
         String[] emailList = new String[1];
         emailList[0] = "u10297902@tuks.co.za";
-        
+
         // Run Test
         boolean result = notificationsBean.sendReminder(emailList);
-        
+
         // Verify Test
         assertTrue(result);
         verify(notificationsBean).buildMessage("u10297902@tuks.co.za", "Reminder");
         verify(notificationsBean).submitTextMail("u10297902@tuks.co.za", "2 Day Reminder", "Test email message");
     }
-    
-    @Test(expected=Exception.class)
-    public void testSendReminderExceptionOnSubmitTextMail() throws Exception
-    {
+
+    @Test(expected = Exception.class)
+    public void testSendReminderExceptionOnSubmitTextMail() throws Exception {
         // Setup
-         doReturn("Test email message").when(notificationsBean).buildMessage(anyString(), anyString());
+        doReturn("Test email message").when(notificationsBean).buildMessage(anyString(), anyString());
         when(notificationsBean.submitTextMail(anyString(), anyString(), anyString())).thenThrow(new Exception());
-        
+
         String[] emailList = new String[1];
         emailList[0] = "u10297902@tuks.co.za";
-        
+
         // Run Test
         boolean result = notificationsBean.sendReminder(emailList);
-        
+
         // Verify Test
         assertFalse(result);
         verify(notificationsBean).buildMessage("u10297902@tuks.co.za", "Reminder");
         verify(notificationsBean).submitTextMail("u10297902@tuks.co.za", "2 Day Reminder", "Test email message");
     }                                                                                                                                                                       // end of SendReminderTest
-}
 
+    @Test
+    public void testSendNotificationBase() throws Exception // start of SendNotificationTest
+    {
+        // Setup
+        doReturn("Test email message").when(notificationsBean).buildMessage(anyString(), anyString());
+        doReturn(true).when(notificationsBean).submitTextMail(anyString(), anyString(), anyString());
+
+        String[] emailList = new String[1];
+        emailList[0] = "u10297902@tuks.co.za";
+
+        // Run Test
+        boolean result = notificationsBean.sendNotification(emailList);
+
+        // Verify Test
+        assertTrue(result);
+        verify(notificationsBean).buildMessage("u10297902@tuks.co.za", "Notification");
+        verify(notificationsBean).submitTextMail("u10297902@tuks.co.za", "Automatic Notification", "Test email message");
+    }
+
+    @Test
+    public void testSendNotificationOneEmailFail() throws Exception {
+        // Setup
+        doReturn("Test email message").when(notificationsBean).buildMessage(anyString(), anyString());
+        doReturn(false).when(notificationsBean).submitTextMail(anyString(), anyString(), anyString());
+
+        String[] emailList = new String[2];
+        emailList[0] = "u10297902@tuks.co.za";
+
+        // Run Test
+        boolean result = notificationsBean.sendNotification(emailList);
+
+        // Verify Test
+        assertFalse(result);
+        verify(notificationsBean).buildMessage("u10297902@tuks.co.za", "Notification");
+        verify(notificationsBean).submitTextMail("u10297902@tuks.co.za", "Automatic Notification", "Test email message");
+    }
+
+    @Test
+    public void testSendNotificationEmptyArray() throws Exception {
+        // Setup
+        String[] emailList = new String[0];
+
+        // Run Test
+        boolean result = notificationsBean.sendNotification(emailList);
+
+        // Verify Test
+        assertFalse(result);
+        verify(notificationsBean, never()).buildMessage(anyString(), anyString());
+        verify(notificationsBean, never()).submitTextMail(anyString(), anyString(), anyString());
+    }
+
+    @Test
+    public void testSendNotificationNullArray() throws Exception {
+        // Setup
+        String[] emailList = null;
+
+        // Run Test
+        boolean result = notificationsBean.sendNotification(emailList);
+
+        // Verify Test
+        assertFalse(result);
+        verify(notificationsBean, never()).buildMessage(anyString(), anyString());
+        verify(notificationsBean, never()).submitTextMail(anyString(), anyString(), anyString());
+    }
+
+    @Test(expected = Exception.class)
+    public void testSendNotificationExceptionOnBuildMessage() throws Exception {
+        // Setup
+        when(notificationsBean.buildMessage(anyString(), anyString())).thenThrow(new Exception());
+        doReturn(true).when(notificationsBean).submitTextMail(anyString(), anyString(), anyString());
+
+        String[] emailList = new String[1];
+        emailList[0] = "u10297902@tuks.co.za";
+
+        // Run Test
+        boolean result = notificationsBean.sendNotification(emailList);
+
+        // Verify Test
+        assertTrue(result);
+        verify(notificationsBean).buildMessage("u10297902@tuks.co.za", "Reminder");
+        verify(notificationsBean).submitTextMail("u10297902@tuks.co.za", "Automatic Notification", "Test email message");
+    }
+
+    @Test(expected = Exception.class)
+    public void testSendNotificationExceptionOnSubmitTextMail() throws Exception {
+        // Setup
+        doReturn("Test email message").when(notificationsBean).buildMessage(anyString(), anyString());
+        when(notificationsBean.submitTextMail(anyString(), anyString(), anyString())).thenThrow(new Exception());
+
+        String[] emailList = new String[1];
+        emailList[0] = "u10297902@tuks.co.za";
+
+        // Run Test
+        boolean result = notificationsBean.sendNotification(emailList);
+
+        // Verify Test
+        assertFalse(result);
+        verify(notificationsBean).buildMessage("u10297902@tuks.co.za", "Reminder");
+        verify(notificationsBean).submitTextMail("u10297902@tuks.co.za", "Automatic Notification", "Test email message");
+    }                                                                                                                                                                       // end of SendNotificationTest
+}
 ///**
 // * Unit test for simple NotificationsBean.
 // */
